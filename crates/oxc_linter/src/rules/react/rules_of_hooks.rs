277539c827bc6b94778f1a665b91f70d2bdc6805
rzvxa <rzvxa@protonmail.com>
2024-04-23 16:15:04 +0330

    test: add more passing tests.

--- crates/oxc_linter/src/rules/react/rules_of_hooks.rs
+++ crates/oxc_linter/src/rules/react/rules_of_hooks.rs
@@ -232,19 +232,19 @@ fn test() {
               useHook();
             }
         ",
-        // // Valid because hooks can call hooks.
-        // "
-        //     function useHook() { useState(); }
-        //     const whatever = function useHook() { useState(); };
-        //     const useHook1 = () => { useState(); };
-        //     let useHook2 = () => useState();
-        //     useHook2 = () => { useState(); };
-        //     ({useHook: () => { useState(); }});
-        //     ({useHook() { useState(); }});
-        //     const {useHook3 = () => { useState(); }} = {};
-        //     ({useHook = () => { useState(); }} = {});
-        //     Namespace.useHook = () => { useState(); };
-        // ",
+        // Valid because hooks can call hooks.
+        "
+            function useHook() { useState(); }
+            const whatever = function useHook() { useState(); };
+            const useHook1 = () => { useState(); };
+            let useHook2 = () => useState();
+            useHook2 = () => { useState(); };
+            ({useHook: () => { useState(); }});
+            ({useHook() { useState(); }});
+            const {useHook3 = () => { useState(); }} = {};
+            ({useHook = () => { useState(); }} = {});
+            Namespace.useHook = () => { useState(); };
+        ",
         // Valid because hooks can call hooks.
         "
             function useHook() {
